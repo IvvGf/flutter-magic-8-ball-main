f35126ac35c67c3f3c133ef17b848654a24ede20
Sofy <A01640091@tec.mx>
2024-05-08 12:13:27 -0600

    titulo

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:custom_app_test/gradient_container.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'Magic 8 ball',
     home: Scaffold(
       body: GradientContainer([Colors.deepPurple, Colors.white]),
     ),
