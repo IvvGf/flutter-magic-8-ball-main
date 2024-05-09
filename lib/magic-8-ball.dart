2a5ff6b410523079331fca35aabb2192fa574385
Sofy <A01640091@tec.mx>
2024-05-08 20:03:16 -0600

    text

--- lib/magic-8-ball.dart
+++ lib/magic-8-ball.dart
@@ -29,6 +29,17 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        const Padding(
+          padding:
+              EdgeInsets.only(left: 20.0, top: 20.0, bottom: 50.0, right: 20.0),
+          child: Text(
+            "Magic-8-Ball reaches into the future, to find the answers to your questions. It knows what will be, and is willing to share this with you.Just think of a question that can be answered \"Yes\""
+            " or \"No\""
+            ", concentrate very, very hard, and click on the \"Obtain an answer\""
+            " button. Then let Magic-8-Ball show you the way!",
+            style: TextStyle(fontStyle: FontStyle.italic),
+          ),
+        ),
         Image.asset(
           'assets/images/dice-$currentDiceRoll.png',
           width: 300,
