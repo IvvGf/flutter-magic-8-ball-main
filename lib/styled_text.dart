353a68a169b8df5bb3a3027dfc95923be007afcd
Sofy <A01640091@tec.mx>
2024-05-08 18:23:03 -0600

    fff

--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -8,18 +8,11 @@ class StyledText extends StatelessWidget {
   @override
   Widget build(context) {
     return Text(
-          text,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        );
-    // return const Column(
-    //   mainAxisAlignment: MainAxisAlignment.center,
-    //   children: [
-    //     Image(image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrmfPVCryBUb3eN06EpdCGNUEUGn3Jv_GhCvLknnR3yg&s')),
-    //     Text('Hola Tonotos', style: TextStyle(color: Colors.white, fontSize: 28),),
-    //   ],
-    // );
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
   }
 }
